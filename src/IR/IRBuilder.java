59f391b09fb8f36b165165d95c51216ff02c9148
irfnfnkemed <irfnfnkemed@sjtu.edu.cn>
2024-06-23 23:34:34 +0800

    feat: add Readme

--- src/IR/IRBuilder.java
+++ src/IR/IRBuilder.java
@@ -411,11 +411,14 @@ public class IRBuilder implements ASTVisitor {
 
     @Override
     public void visit(ForLoop node) {
-        Domain d = new Domain();
-        boolean f = d.getLoop(node);
-        Model m = new Model(d);
-        f = m.setDependency();
-        Schedule s = new Schedule(m);
+        try {
+            Domain d = new Domain();
+            boolean f = d.getLoop(node);
+            Model m = new Model(d);
+            f = m.setDependency();
+            Schedule s = new Schedule(m);
+        } catch (RuntimeException o) {
+        }
         ((FuncDef) now).pushLoop();
         var tmp = ((FuncDef) now).getLoop();
         String condition = var("loopCondition", node.scope.loopPos.line, node.scope.loopPos.column);
